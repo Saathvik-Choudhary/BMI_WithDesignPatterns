46a467f8738142a0e052ad31a3f922e139e57f5f
Zero-definition <saathvikchoudhary@gmail.com>
2024-04-09 13:38:49 +0530

    .

--- src/main/java/org/example/bmi_withdesignpatterns/BMIApplication.java
+++ src/main/java/org/example/bmi_withdesignpatterns/BMIApplication.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Arrays;
 
